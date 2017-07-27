1b24377dc23e2d07b1af2e6568975273f96f4c84
Conor Kelly <ckelly@ckellydesign.net>
2017-07-26 23:00:04 -0700

    Adding FE functionality and init Qube App state

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ## Qübe 
-To serve locally run
-'$ python -m SimpleHTTPServer 9009'
-and open http://localhost:9009
+To serve locally first install:
+'$ npm install'
+
+Then run server:
+'$ node server.js'
+
+and open http://localhost:9009.
+
+___
+### Virtual Qube
+The `index.html` and `app.js` files run the Qube interface and handle the logic of the interaction and to update Firebase with user interaction metrics. A cube virtualization, either through A-Frame or CSS3/JS, can be placed in `index.html` and utilize event listner callback functions defined in `app.js`.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,66 @@
 var QubeApp = function () {
-	console.log('new Qube!')
+	var self = this;
+
+	// QUBE APP STATE
+	this.currentUser = window.users[0];
+
+	this.currentTopic = 'North Korea';
+	this.articles = [ 
+		{
+			article: window.articles[0],
+			userGuess   : "",
+			hasOpened   : false,
+			hasSeenBias : false,
+			timeInArticle : 0
+		},
+		{
+			article: window.articles[1],
+			userGuess   : "",
+			hasOpened   : false,
+			hasSeenBias : false,
+			timeInArticle : 0
+		},
+		{
+			article: window.articles[2],
+			userGuess   : "",
+			hasOpened   : false,
+			hasSeenBias : false,
+			timeInArticle : 0
+		},
+	];
+	this.hasGuessedN = 0;
+	this.hasSavedCurrentArticles = false;
+
+
+	// FIREBASE CONFIG / INIT
+	var config = { // todo - update this
+		apiKey: "AIzaSyDqXCCZmTakDsyMpyzzaX5cV-STTglbIlA",
+		authDomain: "fir-test-bbcbe.firebaseio.com",
+		databaseURL: "fir-test-bbcbe.firebaseio.com",
+		storageKucket: "fir-test-bbcbe.appspot.com"
+	}
+	firebase.initializeApp(config)
+	this.nodeFirebaseAPI = new NodeFirebaseAPI(firebase);
+
+	var valueRef = firebase.database().ref(); // root firebase ref
+	function getValueCallback (snapshot) {
+		// $("#valueText").html(self.value);
+		// var val = snapshot.val()
+		// if (doUpdate && val == self.value) {
+		// 	if (DEBUG) console.log('Uddate Feedback Loop Terminated!!' + self.value + ' === ' + val);
+		// 	return false;
+		// } else {
+		// 	if (DEBUG) console.log('Realtime "value" update');
+		// }
+		// self.value = val;
+		
+
+		// self.nodeFirebaseAPI.updateValue("sliderValue", self.value)
+	}
+
+	valueRef.on("value", getValueCallback);
+
+
 
 
 }
