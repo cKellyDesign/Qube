9281da8599fdcf8cf03a0305144a013d3c66c4d6
Conor Kelly <ckelly@ckellydesign.net>
2017-07-29 21:26:11 -0700

    Unlocking Sources of guessed articles!
    now just to update user stats!

--- js/app.js
+++ js/app.js
@@ -84,19 +84,20 @@ var QubeApp = function () {
 	this.renderScreens = function () {
 
 		$('#left, #center, #right').each(function (i, el) {
+			// defining the article content to render per screen
 			var thisScreen  = self.screens[el.id]
 			var thisAritcle = thisScreen.article;
 
+			// defining author / source per article
 			var thisAuthor = _.findWhere(window.authors, { id : thisAritcle.authorUID})
 			var thisSource = _.findWhere(window.sources, { id : thisAritcle.sourceUID})
 
+			// referencing current qube app screen/source states
 			var thisArticleScreen = self.screens[$(el).attr('id')]
 			var thisSourceScreen =  self.screens[$(el).attr('id') + '_source']
 
 			
-			var thisArticleTemplate = $(el).append($('#ArticleTemplate').clone()).children('svg')
-
-			$(thisArticleTemplate).attr('id', $(thisArticleTemplate).attr('id') + '_' + i)
+			
 
 
 			// Setup Source Sreen
@@ -109,25 +110,36 @@ var QubeApp = function () {
 		$('#top').append($('#SourceScreen_center'))
 	}
 
-	this.renderSourceScreen = function (el, thisSource, thisAritcle) {
-		thisAritcle.el = $('#SourceScreen').clone()
-		$(thisAritcle.el).attr('id', $(thisAritcle.el).attr('id') + '_' + el.id).addClass(el.id)
+	this.renderSourceScreen = function (el, thisSource) {
+		// Create new SVG
+		thisSource.el = $('#SourceScreen').clone()
+
+		// Set SVG to unique ID and add class to orient SVG 
+		// towards corresponding Article Screen
+		$(thisSource.el).attr('id', $(thisSource.el).attr('id') + '_' + el.id).addClass(el.id)
 
 		// Drop that bad gal into the #top div
-		$('#top').append(thisAritcle.el)
+		$('#top').append(thisSource.el)
 
-		// if ($(thisAritcle.el)hasClass(''))
+		// todo - all the customization bits to populate the source info
 
 
-
+		// Listen for a custom event from the view port listening for
+		// the completion of the viewport calculating the new side and
+		// giving it the class active.
 		window.viewport.on('side-change-complete', function (e) {
+			// ignore if the side isn't active
 			if ( !$(el).hasClass('active')) return false;
 
-			$('#top').append(thisAritcle.el)
+			// move the corresponding SVG lower in the DOM to become visible 
+			$('#top').append(thisSource.el)
 		});
 	}
 
 	this. renderArticleSreen = function (el, thisAritcle, i) {
+		// Rende
+		var thisArticleTemplate = $(el).append($('#ArticleTemplate').clone()).children('svg')
+		$(thisArticleTemplate).attr('id', $(thisArticleTemplate).attr('id') + '_' + i)
 		var $guessButton = $('#BiasGuessBtn').attr('id', 'BiasGuessBtn_' + i)
 
 		// Adding event listener (for some reason it doesn't like .on('click', this.callbackFunc); )
@@ -160,7 +172,7 @@ var QubeApp = function () {
 
 	this.showBiasGuessOverlay = function (el) {
 		if (!$('#GuessOverlay', el).length) {
-			 $(el).append($('#GuessOverlay').clone())
+			 $(el).append($('#svgTemplateWrap #GuessOverlay').clone())
 		} else {
 			$(el).append($('#GuessOverlay', el))
 			$('#GuessOverlay', el).removeClass('ghost')
@@ -181,6 +193,25 @@ var QubeApp = function () {
 		})
 		
 	}
+
+	this.updateSourceScreenEl = function (el, bias) {
+		// define svg elements to move around
+		var svg = $('#SourceScreen_' + el.id)
+		var biasPie = $('#Source #' + bias, svg)
+		var biasBG = $('#Borders #Border_' + bias, svg)
+
+		// move SVGs up and down through dom to hide / reveal them
+		svg.prepend($('#Locked_Pie', svg)).append(biasPie)
+		
+		// move correct bias to bottom of DOM in coorispoding source
+		// to reveal the right answer
+		$('#Borders', svg).append(biasBG)
+
+	}
+
+
+
+
 	// END CSS / JS / SVG Implementation
 
 
@@ -209,6 +240,9 @@ var QubeApp = function () {
 
 
 		self.screens[el.id + '_source'].isLocked = false; // do we unlock them all at once?
+
+		self.updateSourceScreenEl(el, correctBias)
+
 		self.biasGuessCount++;
 		if (self.biasGuessCount === this.articles.length) {
 			//unlock next round of articles!
